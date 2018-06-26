3d134ee201b7c8ca92fc8b077e6378595bae0b9d
Ej Corpuz <ejan.corp@gmail.com>
2018-06-26 21:05:47 +0400

    update files

---
+++ gulpfile.babel.js
@@ -0,0 +1,23 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+
+gulp.task('default', ['compile']);
+
+gulp.task('watch', () => {
+    gulp.watch('src/**/*.js', ['compile']);
+});
+
+gulp.task('compile', () => {
+    return gulp.src('src/**/*.js')
+        .pipe(concat('drag-en-select.js'))
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(uglify())
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(gulp.dest('dist'))
+});
